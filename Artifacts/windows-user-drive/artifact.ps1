d1a8998910929964ae809a4a6bc41ca0260265d6
Elizabeth Maher (NEWMAN) <enewman@microsoft.com>
2019-06-13 23:51:51 +0000

    saving work

--- Artifacts/windows-user-drive/artifact.ps1
+++ Artifacts/windows-user-drive/artifact.ps1
@@ -52,11 +52,9 @@ trap
 $MaxRetries = 30
 $currentRetry = 0
 $success = $false
-$KeyVaultName = "testkeyvault"
+$KeyVaultName = "filelab6434"
 
 Write-Host "Start: " + $(Get-Date)
-
-return
 
 
 do {
@@ -67,12 +65,18 @@ do {
             throw "The current version of PowerShell is $($PSVersionTable.PSVersion.Major). Prior to running this artifact, ensure you have PowerShell 3 or higher installed."
         }
         
+
+		Write-Host "Start: get vm identity"
+
         # Get KeyVault token as the VM identity       
         $response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Method GET -Headers @{Metadata="true"} -UseBasicParsing
         Write-Host "Success: " + $(Get-Date)
         $content = $response.Content | ConvertFrom-Json
         $KeyVaultToken = $content.access_token
 
+		Write-Host "End: get vm identity"
+
+		
         # Get credentials
         $result = (Invoke-WebRequest -Uri "https://$KeyVaultName.vault.azure.net/secrets/TestAccountCredential?api-version=2016-10-01" -Method GET -Headers @{Authorization="Bearer $KeyVaultToken"} -UseBasicParsing).content
         $begin = $result.IndexOf("value") + 8
